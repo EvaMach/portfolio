99d1ff8b6d02ad1190a8d8844b758543245d7b3d
EvaMach <evca.machova@gmail.com>
2025-02-16 12:17:01 +0100

    Init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Eva Machová - Frontend Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable}`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 left-0 w-full py-8 xl:py-12'>
+    </header>
+  );
+};
+
+export default Header;
