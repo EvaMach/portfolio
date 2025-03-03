d70d967b37e02a039cad41162859e11508fc7c56
EvaMach <evca.machova@gmail.com>
2025-03-03 23:36:44 +0100

    Add logo and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import React from 'react';
-import { MobileNav } from './MobileNav';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
@@ -10,8 +9,10 @@ const Header = () => {
   const pathName = usePathname();
   return (
     <header className='sticky top-0 bg-background opacity-90 z-10 flex justify-between items-center my-4 mx-5 sm:mx-10 md:mx-[15%]'>
-      <Link href="#about"><Image src="/img/logo.png" alt="Logo" width={70} height={70} /></Link>
-      <nav className='hidden lg:flex'>
+      <Link href="#about">
+        <Image src="/img/logo.png" alt="Logo" width={80} height={80} />
+      </Link>
+      <nav className='flex'>
         {pathName === '/' ? (
           <ul className='flex gap-4'>
             <li><Link href="#about">About</Link></li>
@@ -23,10 +24,6 @@ const Header = () => {
         )
         }
       </nav>
-
-      <div className='lg:hidden'>
-        <MobileNav />
-      </div>
     </header >
   );
 };
