f88ee35ce0f803a6531c027255807357c1e9514a
EvaMach <evca.machova@gmail.com>
2025-03-05 00:08:28 +0100

    Fix sonner component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,27 +10,27 @@ const About = () => {
       <div className='relative flex items-center justify-between gap-8'>
         <div>
           <h1 className='mb-8'>Hi there,<br />I&apos;m <span className='text-accent'>Eva</span> Machová</h1>
-          <p className='mb-4'>I am a <span className='font-semibold'>user-focused frontend-developer </span>
+          <p className='mb-4 lg:text-xl'>I am a <span className='font-semibold'>user-focused frontend-developer </span>
             with strong background in <span className='font-semibold'>QA</span> and <span className='font-semibold'>UX/UI</span> design.</p>
-          <p>An advocate for clean, maintainable code, I enjoy
+          <p className='mb-4 lg:text-xl'>An advocate for clean, maintainable code, I enjoy
             making projects pretty not just on the outside. ✨</p>
         </div>
         <Image
           sizes='(max-width: 768px) 33vw, (max-width: 1200px) 33vw, 18rem'
           className="hidden xs:block w-1/3 min-w-40 max-w-72" src={myPic} alt="Girl coding" />
       </div>
-      <div className='flex gap-4 items-center mt-4'>
+      <div className='flex gap-4 items-center'>
         <Link className='hover:text-accent text-xl' href="https://www.linkedin.com/in/eva-machova-frontend-developer/">
-          <LinkedinIcon className='w-8 h-8 fill-white hover:fill-gray-400' />
+          <LinkedinIcon className='w-7 h-7 fill-white hover:fill-gray-400' />
         </Link>
         <Link href="https://github.com/EvaMach">
-          <GitHubIcon className='fill-white hover:fill-gray-400 w-8 h-8' />
+          <GitHubIcon className='fill-white hover:fill-gray-400 w-7 h-7' />
         </Link>
       </div>
 
-      <h2 className='mt-8'>About</h2>
-      <p className='mb-4 lg:text-lg'>My path to IT has not been conventional. Although I have a Master&apos;s degree in Linguistics and English Language, I&apos;ve always felt drawn to technology. As a result I decided to take a job as a tester and discovered my <span className='font-semibold'>passion for ensuring quality and great user experience</span> in web applications. I explored UX/UI design for a while, conducting user interviews, usability testing, and creating prototypes. But soon, I realized that I want to bring those designs to life myself - and so I took the plunge into <span className='font-semibold'>frontend development.</span></p>
-      <p className='mb-4 lg:text-lg'>I took the Czechitas Digital Academy intensive course in Web development which helped me transition into my current role as a frontend developer. Now, I’m very happy to be able to <span className='font-semibold'>transform creative, impactful ideas into web applications.</span> I’m always eager to take on new challenges, explore latest technologies, and continue growing. 🚀</p>
+      <h2 className='mt-12'>About</h2>
+      <p className='mb-4 lg:text-lg text-justify'>My path to IT has not been conventional. Although I have a Master&apos;s degree in Linguistics and English Language, I&apos;ve always felt drawn to technology. As a result I decided to take a job as a tester and discovered that I really love collaborating on web apps that provide <span className='font-semibold'>quality and great user experience</span>. I explored UX/UI design for a while, conducting user interviews, usability testing, and creating prototypes. But soon, I realized that I want to be the one to code those designs - and so I dived headfirst into <span className='font-semibold'>frontend development.</span></p>
+      <p className='mb-4 lg:text-lg text-justify'>I took the Czechitas Digital Academy intensive course in Web development which helped me transition into my current role as a frontend developer. Now, enjoy<span className='font-semibold'>turning creative and impactful ideas into web applications.</span> I’m always eager to take on new challenges, explore latest technologies, and continue growing. 🚀</p>
     </section>
   );
 };
