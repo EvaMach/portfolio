e7f9b353b2de7cac1ebe8f4c1d06a62ebcc90af6
Eva Machova <evca.machova@gmail.com>
2025-10-21 20:41:55 +0200

    Fix project image on small screens

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,10 +4,10 @@ import { projects } from "@/lib/projects";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { useParams } from 'next/navigation';
+import { useParams } from "next/navigation";
 
 export default function ProjectDetailPage() {
-  const params = useParams<{ id: string; }>();
+  const params = useParams<{ id: string }>();
   const project = projects.find((p) => p.id === params.id);
 
   if (!project) {
@@ -23,24 +23,52 @@ export default function ProjectDetailPage() {
         <div className="flex justify-between">
           <h2 className="my-4">{project.title}</h2>
           <div className="hidden md:block text-right">
-            <div className='text-gray-400'>{`${project.year} ${project.client && `• ${project.client}`}`}</div>
+            <div className="text-gray-400">{`${project.year} ${
+              project.client && `• ${project.client}`
+            }`}</div>
             {project.link && <Link href={project.link}>{project.link}</Link>}
           </div>
         </div>
         <div className="grid gap-2 border-b-2 pb-4 border-accent mb-4">
-          <div><span className="text-gray-300 font-semibold">Roles: </span>{project.roles}</div>
-          <div><span className="text-gray-300 font-semibold">Team size: </span>{project.team}</div>
-          <div><span className="text-gray-300 font-semibold">Tech stack: </span>{project.techStack.join(", ")}</div>
-          <div><span className="text-gray-300 font-semibold">Frameworks & Libraries: </span>{project.libs.join(", ")}</div>
-          <div><span className="text-gray-300 font-semibold">Tools: </span>{project.tools.join(", ")}</div>
+          <div>
+            <span className="text-gray-300 font-semibold">Roles: </span>
+            {project.roles}
+          </div>
+          <div>
+            <span className="text-gray-300 font-semibold">Team size: </span>
+            {project.team}
+          </div>
+          <div>
+            <span className="text-gray-300 font-semibold">Tech stack: </span>
+            {project.techStack.join(", ")}
+          </div>
+          <div>
+            <span className="text-gray-300 font-semibold">
+              Frameworks & Libraries:{" "}
+            </span>
+            {project.libs.join(", ")}
+          </div>
+          <div>
+            <span className="text-gray-300 font-semibold">Tools: </span>
+            {project.tools.join(", ")}
+          </div>
         </div>
         <div className="flex flex-col justify-center items-center gap-4">
           <p className="mt-2 text-justify">{project.longDescription[0]}</p>
-          <Image className="rounded-lg max-w-lg" src={project.image} alt={`${project.title} app screenshot`} />
+
+          <div className="max-w-lg">
+            <Image
+              className="rounded-lg w-full"
+              src={project.image}
+              alt={`${project.title} app screenshot`}
+            />
+          </div>
           <p className="mt-2 text-justify">{project.longDescription[1]}</p>
         </div>
         <div>
-          <h3 className="text-xl font-semibold my-4">Contributions and proud moments 💫</h3>
+          <h3 className="text-xl font-semibold my-4">
+            Contributions and proud moments 💫
+          </h3>
           <ul className="list-disc marker:accent pl-4 space-y-2">
             {project.contributions.map((contribution, index) => (
               <li key={index}>{contribution}</li>
